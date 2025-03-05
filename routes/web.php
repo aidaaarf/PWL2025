ca40a498ced39e638c414b20581395f62b31461c
aidas777-ctrl <fadhila2903@gmail.com>
2025-03-05 20:17:24 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/greeting', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/NIM', function(){
+    return '2341720094';
+});
+
+Route::get('/user/{name}', function($name){
+    return 'Nama Saya '. $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId){
+    return 'Pos ke-'. $postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function
+($articleId){
+    return 'Halaman Artikel dengan ID '. $articleId;
+});
+
+Route::get('/user/{name?}', function($name=null){
+    return 'Nama Saya '. $name;
+});
+
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama Saya '. $name;
+});
